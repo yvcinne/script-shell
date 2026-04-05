e321e5045e90bfd028a7e0587ecedacd3abe1824
yvcinne <elkarramsi@gmail.com>
2026-04-05 22:51:14 +0100

    fix typo

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# monitoring.sh - Surveillance periodique du CPU et de la memoire
-# Prelèvements toutes les 0.5 secondes (sleep 0.5)
 
-echo "Demarrage du monitoring (Ctrl+C pour arreter)"
+
+echo "Démarrage du monitoring (Ctrl+C pour arrêter)"
 echo "----------------------------------------------"
 
 while true; do
@@ -12,11 +11,11 @@ while true; do
     # Charge CPU : pourcentage d'utilisation (100 - idle)
     cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 
-    # Memoire disponible en Mo
+    # Mémoire disponible en Mo
     mem_dispo=$(free -m | awk '/^Mem:/ {print $7}')
     mem_total=$(free -m | awk '/^Mem:/ {print $2}')
 
-    echo "[$heure] CPU utilise : ${cpu}%  |  Memoire disponible : ${mem_dispo} Mo / ${mem_total} Mo"
+    echo "[$heure] CPU utilisé : ${cpu}%  |  Mémoire disponible : ${mem_dispo} Mo / ${mem_total} Mo"
 
     sleep 0.5
 done
