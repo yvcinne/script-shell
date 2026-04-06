79010dcf98899cf4c3944937a8e97ba46fdccd74
yvcinne <elkarramsi@gmail.com>
2026-04-06 08:49:38 +0100

    update

---
+++ analyse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+echo "Bonjour, vous avez fourni $# paramètre(s)."
+
+echo "Le nom du script est $0 "
+
+echo "Le 3e paramètre est $3 "
+
+echo "Voici la liste des paramètres $* "
